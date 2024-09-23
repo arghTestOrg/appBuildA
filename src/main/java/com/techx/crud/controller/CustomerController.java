5452577ad11b14e990472f2242e2806fdaa08573
Kishor <aaaaarrrgghhh@yahoo.com>
2024-09-23 15:21:14 +0800

    prettified html, added update

--- src/main/java/com/techx/crud/controller/CustomerController.java
+++ src/main/java/com/techx/crud/controller/CustomerController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/customers")
@@ -22,6 +23,20 @@ public class CustomerController {
     @PostMapping
     public Customer addCustomer(@RequestBody Customer customer) {
         return customerRepository.save(customer);
+    }
+
+    // Update an existing customer
+    @PutMapping("/{id}")
+    public Customer updateCustomer(@PathVariable String id, @RequestBody Customer updatedCustomer) {
+        Optional<Customer> optionalCustomer = customerRepository.findById(id);
+        if (optionalCustomer.isPresent()) {
+            Customer existingCustomer = optionalCustomer.get();
+            existingCustomer.setName(updatedCustomer.getName());
+            existingCustomer.setEmail(updatedCustomer.getEmail());
+            return customerRepository.save(existingCustomer);
+        } else {
+            throw new RuntimeException("Customer not found with id: " + id);
+        }
     }
 
     @DeleteMapping("/{id}")
